d3b5ca56e4196191d0dead19ecb067e74fa5524a
Abhishek Prakash <abhishekprakash2308@gmail.com>
2021-07-02 16:04:53 +0530

    Books create index new

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
+
 // All authors route
 router.get("/", async (req, res) => {
   let searchOptions = {};
@@ -20,7 +21,7 @@ router.get("/new", (req, res) => {
   res.render("authors/new", { author: new Author() });
 });
 
-// Creating new author
+// Creating new author route
 router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
