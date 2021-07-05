ce7f7ebce13164b650b6dd97eaba7af43a8c817a
Abhishek Prakash <abhishekprakash2308@gmail.com>
2021-07-05 19:56:12 +0530

    finished book routes

--- routes/books.js
+++ routes/books.js
@@ -1,80 +1,158 @@
-const express = require('express')
-const router = express.Router()
-const Book = require('../models/book')
-const Author = require('../models/author')
-const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book");
+const Author = require("../models/author");
+const imageMimeTypes = ["image/jpeg", "image/png", "images/gif"];
 
 // not using multer anymore to upload file to the server as heroku deletes all the file uploaded everyday
 
 // All Books Route
-router.get('/', async (req, res) => {
-  let query = Book.find()
-  if (req.query.title != null && req.query.title != '') {
-    query = query.regex('title', new RegExp(req.query.title, 'i'))
+router.get("/", async (req, res) => {
+  let query = Book.find();
+  if (req.query.title != null && req.query.title != "") {
+    query = query.regex("title", new RegExp(req.query.title, "i"));
   }
-  if (req.query.publishedBefore != null && req.query.publishedBefore != '') { //lte less than equal to
-    query = query.lte('publishDate', req.query.publishedBefore)
+  if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
+    //lte less than equal to
+    query = query.lte("publishDate", req.query.publishedBefore);
   }
-  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-    query = query.gte('publishDate', req.query.publishedAfter)
+  if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
+    query = query.gte("publishDate", req.query.publishedAfter);
   }
   try {
-    const books = await query.exec()
-    res.render('books/index', {
+    const books = await query.exec();
+    res.render("books/index", {
       books: books,
-      searchOptions: req.query
-    })
+      searchOptions: req.query,
+    });
   } catch {
-    res.redirect('/')
+    res.redirect("/");
   }
-})
+});
 
 // New Book Route
-router.get('/new', async (req, res) => {
-  renderNewPage(res, new Book())
-})
+router.get("/new", async (req, res) => {
+  renderNewPage(res, new Book());
+});
 
 // Create Book Route
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    description: req.body.description
-  })
-  saveCover(book, req.body.cover)
+    description: req.body.description,
+  });
+  saveCover(book, req.body.cover);
 
   try {
-    const newBook = await book.save()
-    // res.redirect(`books/${newBook.id}`)
-    res.redirect(`books`)
+    const newBook = await book.save();
+    res.redirect(`books/${newBook.id}`);
   } catch {
-    renderNewPage(res, book, true)
+    renderNewPage(res, book, true);
   }
-})
+});
+
+// Show book route
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id).populate("author").exec(); // populate will fill all the fields for book, without that we will just get id
+    res.render("books/show", { book: book });
+  } catch {
+    res.render("/");
+  }
+});
+
+// Edit Book Route
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    renderEditPage(res, book);
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// Update Book Route
+router.put("/:id", async (req, res) => {
+  let book;
+
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publlishDate = new Date(req.body.publishDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+    if (req.body.cover != null && req.body.cover !== "") {
+      saveCover(book, req.body.cover);
+    }
+    await book.save();
+    res.redirect(`books/${book.id}`);
+  } catch {
+    if (book != null) {
+      renderEditPage(res, book, true);
+    } else {
+      redirect("/");
+    }
+  }
+});
+
+// Delete book route
+router.delete("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    await book.remove();
+    res.redirect("/books");
+  } catch {
+    if (book != null) {
+      res.render("books/show", {
+        book: book,
+        errorMessage: "Could not remove book",
+      });
+    } else {
+      res.redired("/");
+    }
+  }
+});
 
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, "new", hasError);
+}
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, "edit", hasError);
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
   try {
-    const authors = await Author.find({})
+    const authors = await Author.find({});
     const params = {
       authors: authors,
-      book: book
+      book: book,
+    };
+    if (hasError) {
+      if (form === "edit") {
+        params.errorMessage = "Error Updating Book";
+      } else {
+        params.errorMessage = "Error Creating Book";
+      }
     }
-    if (hasError) params.errorMessage = 'Error Creating Book'
-    res.render('books/new', params)
+    res.render(`books/${form}`, params);
   } catch {
-    res.redirect('/books')
+    res.redirect("/books");
   }
 }
 
-function saveCover(book, coverEncoded){
-  if(coverEncoded == null) return
-  const cover = JSON.parse(coverEncoded)
-  if(cover != null && imageMimeTypes.includes(cover.type)){
-    book.coverImage = new Buffer.from(cover.data, 'base64')
-    book.coverImageType = cover.type
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, "base64");
+    book.coverImageType = cover.type;
   }
 }
 
-module.exports = router
+module.exports = router;
