213c6c5e8b33dd423cf15f10531b6daf78b93b36
Abhishek Prakash <abhishekprakash2308@gmail.com>
2021-07-10 18:46:07 +0530

    fixed filepond image preview issue

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,27 +1,33 @@
 const rootStyles = window.getComputedStyle(document.documentElement); /* gets all the styles from the root (:root tag) element of the document (books.css)  */
 
-if(rootStyles.getPropertyValue('--book-cover-width-large') != null && 
-    rootStyles.getPropertyValue('--book-cover-width-large') !== ''){/* this javascript file loads before the css files so we need to check if the variable is available first */
-    ready();
+if (
+  rootStyles.getPropertyValue("--book-cover-width-large") != null &&
+  rootStyles.getPropertyValue("--book-cover-width-large") !== ""
+) { /* this javascript file loads before the css files so we need to check if the variable is available first */
+  ready();
 } else {
-    document.getElementById('main-css').addEventListener('load', ready);
+  document.getElementById("main-css").addEventListener("load", ready);
 }
 
-function ready(){
-    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'));
-    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'));
-    const coverHeight = coverWidth / coverAspectRatio;
+function ready() {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-width-large")
+  );
+  const coverAspectRatio = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-aspect-ratio")
+  );
+  const coverHeight = coverWidth / coverAspectRatio;
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+  );
 
-    FilePond.registerPlugin(
-        FilePondPluginImagePreview,
-        FilePondPluginImageResize,
-        FilePondPluginFileEncode
-    )
-    
-    FilePond.setOptions({
-        stylePanelAspectRatio: 1 / coverAspectRatio,
-        imageResizeTargetWidth: coverWidth,
-        imageResizeTargetHeight: coverHeight
-    })
-    FilePond.parse(document.body);
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
+  });
+
+  FilePond.parse(document.body);
 }
